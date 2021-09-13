b34063d59ee586040cf0f50004a96d86d920a5d3
milap91 <mvs7891@gmail.com>
2021-09-14 01:24:34 +0530

    added fixture at class level

--- Tests/Test_endtoend.py
+++ Tests/Test_endtoend.py
@@ -1,31 +1,34 @@
 import time
 
+import pytest
 from selenium import webdriver
 
 from PageObjects.GreenCart_HomePage import GreenCart_HomePage
 from PageObjects.PromoCodePage import PromoCodePage
 from PageObjects.TermsConditionPage import TermsConditionPage
+from Tests.conftest import setup
 
 
-def test_e2e1():
-    driver = webdriver.Chrome(executable_path="C:\\Users\\milap\\OneDrive\\Desktop\\selenium\\chromedriver.exe")
-    driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
-    Homepage = GreenCart_HomePage(driver)
-    Homepage.search_product("berry")
-    Homepage.verify_products_count(2)
-    Homepage.verify_products_name_and_add_them_in_cart()
-    Homepage.click_on_cartitem()
-    Homepage.verify_products_count_in_cart(2)
-    Homepage.verify_products_name_in_cart()
-    Homepage.click_on_proceed_to_checkout()
-    BillingPage = PromoCodePage(driver)
-    BillingPage.verify_products_count_in_product_table(2)
-    BillingPage.verify_products_name_in_product_table()
-    BillingPage.enter_promocode("rahulshettyacademy")
-    BillingPage.verify_discount("10%")
-    BillingPage.click_on_place_order()
-    time.sleep(3)
-    Termspage = TermsConditionPage(driver)
-    Termspage.Accept_Conditions("India")
-    Termspage.verify_success_Text()
-    time.sleep(3)
+@pytest.mark.usefixtures("setup")
+class Test_endtoend:
+    def test_e2e1(self):
+
+        Homepage = GreenCart_HomePage(self.driver)
+        Homepage.search_product("berry")
+        Homepage.verify_products_count(2)
+        Homepage.verify_products_name_and_add_them_in_cart()
+        Homepage.click_on_cartitem()
+        Homepage.verify_products_count_in_cart(2)
+        Homepage.verify_products_name_in_cart()
+        Homepage.click_on_proceed_to_checkout()
+        BillingPage = PromoCodePage(self.driver)
+        BillingPage.verify_products_count_in_product_table(2)
+        BillingPage.verify_products_name_in_product_table()
+        BillingPage.enter_promocode("rahulshettyacademy")
+        BillingPage.verify_discount("10%")
+        BillingPage.click_on_place_order()
+        time.sleep(3)
+        Termspage = TermsConditionPage(self.driver)
+        Termspage.Accept_Conditions("India")
+        Termspage.verify_success_Text()
+        time.sleep(3)
